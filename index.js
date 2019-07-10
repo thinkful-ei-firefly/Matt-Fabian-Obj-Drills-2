fb61501499ed06c8eff619011b7e2d54a72905d7
Fabian Lema <fabianlemac@gmail.com>
2019-07-10 11:06:54 -0500

    Added enrollInSummerSchool function

--- index.js
+++ index.js
@@ -11,3 +11,34 @@ const testData = [
 ];
 
 console.log(makeStudentReports(testData));
+
+
+
+const studentData = [
+  {
+    name: 'Tim',
+    status: 'Current student',
+    course: 'Biology',
+  },
+  {
+    name: 'Sue',
+    status: 'Withdrawn',
+    course: 'Mathematics',
+  },
+  {
+    name: 'Liz',
+    status: 'On leave',
+    course: 'Computer science',
+  },
+];
+
+function enrollInSummerSchool(students) {
+  // your code here
+  return students.map(student => {
+  	student.status = 'In Summer school'
+  	return student;
+  });
+}
+
+console.log(enrollInSummerSchool(studentData));
+
