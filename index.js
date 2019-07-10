3b44c68870a10fd668c62e569bc45be60cf06733
Matthew Mbonu <mcmbonu1@gmail.com>
2019-07-10 12:00:09 -0400

    makeStudentReports

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-const var = "var"
+const makeStudentReports = arr => {
+  const report = [];
+  arr.forEach(student => report.push(`${student.name}: ${student.grade}`));
+  return report;
+};
+
+const testData = [
+  { name: "Jane Doe", grade: "A" },
+  { name: "John Dough", grade: "B" },
+  { name: "Jill Do", grade: "A" }
+];
+
+console.log(makeStudentReports(testData));
